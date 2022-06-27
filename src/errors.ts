739347f884b2fcbb30874ef127d81a639230f5b9
Mathis Tailland <mathis.tailland@gmail.com>
2022-06-27 09:18:51 +0200

    add error name

--- src/errors.ts
+++ src/errors.ts
@@ -2,6 +2,7 @@ export class XMLConversionError extends Error {
   origin: unknown;
   constructor(message: string, origin?: XMLConversionError["origin"]) {
     super(message);
+    this.name = "XMLConversionError";
     this.origin = origin;
   }
 }
