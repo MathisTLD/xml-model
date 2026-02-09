958cd05896e2869e909987962ba97749f8a4935a
Mathis Tailland <mathis.tailland@gmail.com>
2026-02-09 11:06:15 +0100

    feat: re-export API from typescript-rtti

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ export { defaults } from "./defaults";
 export { getModel, createModel, Model, Prop } from "./model";
 export * from "./errors";
 
+export * from "typescript-rtti";
+
 import XML from "./xml";
 export { XML };
