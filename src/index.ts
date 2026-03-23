90e51ec3e3eb1512746745ced5416ff839f7e17c
Mathis Tailland <mathis.tailland@gmail.com>
2026-03-23 18:31:31 +0100

    fix: re-export everything from models.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export { model, isModel } from "./model";
-export type { ModelConstructor } from "./model";
-
+// FIXME: strange bug, If not exporting all from ./model
+// then can't import data from `xml-model/model` even if
+// in source there is `export const DATA...`
+export * from "./model";
 export * from "./xml";
