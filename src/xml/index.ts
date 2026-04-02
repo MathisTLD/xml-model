cc10d4da318f79d4dfa77bf9ca2d19ece0e8b0b9
Mathis Tailland <mathis.tailland@gmail.com>
2026-04-02 16:07:39 +0200

    fix: patch export of `xmlCodec`

--- src/xml/index.ts
+++ src/xml/index.ts
@@ -2,5 +2,11 @@ export * from "./xml-js";
 
 export { xml } from "./schema-meta";
 export type { UserCodecOptions, XMLState } from "./codec";
-export { registerDefault, normalizeCodecOptions, XMLCodecError, xmlStateSchema } from "./codec";
+export {
+  xmlCodec, // if not re-exported here, xmlCodec is not exported from "./codex.js" but only from "./codec.d.ts"
+  registerDefault,
+  normalizeCodecOptions,
+  XMLCodecError,
+  xmlStateSchema,
+} from "./codec";
 export { xmlModel, type XmlModelConstructor } from "./model";
